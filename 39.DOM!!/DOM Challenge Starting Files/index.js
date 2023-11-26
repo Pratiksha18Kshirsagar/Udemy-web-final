04d743bd3fa983cef4b6d7acf799a65d02cd4640
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-11-26 11:52:30 +0530

    26-11-2023

--- 39.DOM!!/DOM Challenge Starting Files/index.js
+++ 39.DOM!!/DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
-document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+document.getElementsByTagName("h1")[0].style.color = "pink";
+// document.getElementsByClassName("btn")
+// document.getElementById("name")
+document.querySelector(".btn" ).style.   backgroundColor = "pink" ;
+// document.querySelectorAll("li")
+document.querySelector("li a").setAttribute("href" ,"https://www.bing.com/");
+document.querySelector("h1").textContent = "byebye" ;
